9ea2a025cfe2033e3e25722a6a6fe42bce9b6338
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-10-23 18:11:29 +0100

    feat: add DI computataion script

--- scripts/utils.R
+++ scripts/utils.R
@@ -4,3 +4,15 @@ getSC <- function(data, dataCluster) {
   sum <- summary(sc)
   return(sum$avg.width)
 }
+
+getDI <- function(data, dataCluster) {
+
+    intraclust = c("complete","average","centroid")
+    interclust = c("single", "complete", "average","centroid",
+    "aveToCent", "hausdorff")
+    df = as.data.frame(data)
+    cls.scatt <- cls.scatt.data(df, dataCluster$cluster,
+    dist="euclidean")
+    dunn <- clv.Dunn(cls.scatt, c("average"), c("average"))
+    return(dunn[1])
+}
